dca097b4a0608ee4a1b394197a116b145f8e27f4
AntonBertelsen <anton.bertelsen@gmail.com>
2021-10-26 11:27:02 +0200

    Initial commit

--- client/client.go
+++ client/client.go
@@ -1,1 +1,79 @@
-package Mini_Projekt_Dis_Sys
+package main
+
+import (
+	"bufio"
+	"context"
+	"fmt"
+	"google.golang.org/grpc"
+	"io"
+	"log"
+	pb "mini_project_dis_sys"
+	"os"
+	"time"
+)
+
+const (
+	address = "localhost:50051"
+)
+
+func main() {
+	conn, err := grpc.Dial(address, grpc.WithInsecure(), grpc.WithBlock())
+	if err != nil {
+		log.Fatalf("Did not connect: %v", err)
+	}
+	defer conn.Close()
+	c := pb.NewChatServiceClient(conn)
+
+	scanner := bufio.NewScanner(os.Stdin)
+	fmt.Println("Enter your chat name:")
+	scanner.Scan()
+	name := scanner.Text()
+
+	// create stream
+	client := pb.NewChatServiceClient(conn)
+	in := &pb.JoinMessage{Sender: name}
+	stream, err := client.Subscribe(context.Background(), in)
+	if err != nil {
+		log.Fatalf("Subscribe error %v", err)
+	}
+
+	done := make(chan bool)
+
+	go func() {
+		for {
+			resp, err := stream.Recv()
+			if err == io.EOF {
+				done <- true //means stream is finished
+				return
+			}
+			if err != nil {
+				log.Fatalf("cannot receive %v", err)
+			}
+			log.Printf("Resp received: %s", resp.Message)
+		}
+	}()
+
+	<-done //we will wait until all response is received
+	log.Printf("finished")
+
+	for {
+		scanner.Scan()
+
+		if len(scanner.Text()) <= 128 {
+			ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+			defer cancel()
+
+			_, err := c.Publish(ctx, &pb.ChatMessage{
+				T:       0,
+				Sender:  name,
+				Message: scanner.Text(),
+			})
+			if err != nil {
+				log.Printf("could not send the message: %v", err)
+			}
+		} else {
+			fmt.Println("Max message length is 128 characters")
+		}
+	}
+
+}
--- server/server.go
+++ server/server.go
@@ -1,1 +1,65 @@
-package client
+package main
+
+import (
+	"context"
+	"fmt"
+	"google.golang.org/grpc"
+	"log"
+	pb "mini_project_dis_sys"
+	"net"
+	"sync"
+)
+
+const (
+	port = ":50051"
+)
+
+type ChatServiceServer struct {
+	pb.UnimplementedChatServiceServer
+}
+
+func (s *ChatServiceServer) Publish(ctx context.Context, in *pb.ChatMessage) (*pb.ChatMessage, error) {
+	fmt.Printf("Received message: %v %v \n", in.GetSender(), in.GetMessage())
+	msgChannel <- *in
+	return in, nil
+}
+
+var msgChannel = make(chan pb.ChatMessage)
+
+func (s *ChatServiceServer) Subscribe(in *pb.JoinMessage, srv pb.ChatService_SubscribeServer) error {
+	fmt.Printf("Client connected: %v", in.Sender)
+
+	var wg sync.WaitGroup
+
+	wg.Add(1)
+	go func() {
+
+		for {
+			defer wg.Done()
+			fmt.Println("test!")
+			message := <-msgChannel
+			fmt.Printf("I GOT A MESSAGE IT WORKS!!")
+			err := srv.Send(&message)
+			if err != nil {
+				log.Printf("send error %v", err)
+			}
+		}
+	}()
+
+	wg.Wait()
+	return nil
+}
+
+func main() {
+	lis, err := net.Listen("tcp", port)
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	s := grpc.NewServer()
+	pb.RegisterChatServiceServer(s, &ChatServiceServer{})
+	log.Printf("server listening at %v", lis.Addr())
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
